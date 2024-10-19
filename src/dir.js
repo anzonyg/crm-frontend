f6c3dbdff45e4f62e418ac2f6b51904a809177ad
anzonyg <anzonyg@live.com>
2024-10-19 01:48:33 -0600

    actualizacion url backend

--- src/dir.js
+++ src/dir.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = 'http://localhost:4000/tasks/';
+/* const API_BASE_URL = 'http://localhost:4000/tasks/'; */
+const API_BASE_URL = 'https://crm-backend-main-7cec.onrender.com/tasks';
+
 
 export default API_BASE_URL;
