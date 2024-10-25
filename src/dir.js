cc8520dd73ab751b3405da92b21781ca30161a52
anzonyg <anzonyg@live.com>
2024-10-24 20:53:39 -0600

    redireccionar al servidor web

--- src/dir.js
+++ src/dir.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://localhost:4000/tasks/';
-/* const API_BASE_URL = 'https://crm-backend-main-7cec.onrender.com/tasks/'; */
+/* const API_BASE_URL = 'http://localhost:4000/tasks/'; */
+const API_BASE_URL = 'https://crm-backend-main-7cec.onrender.com/tasks/';
 
 
 export default API_BASE_URL;
